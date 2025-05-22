8c38ede0f92e4e135018c71aac6fe4fe3f388a42
hpozza <henriquepza@gmail.com>
2025-05-22 15:30:06 -0300

    Added modal, tree visualization and images for visualization

--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { PlotModal } from '../../components/PlotModal'
 
 export const TimelineContainer = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: url("/src/assets/background_timeline.png");
+    background-image: url("/src/assets/images/background_timeline.png");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 160px 164px;
 `
 
 export default function Timeline() {
   return (
-    <TimelineContainer />
+    <TimelineContainer>
+      <PlotModal/>
+    </TimelineContainer>
   )
 }
---
+++ src/utils/TimelineProps.ts
@@ -0,0 +1,37 @@
+export const plot = [
+  {
+    id: 1,
+    title: "Plot 1",
+    description:
+      "Kuwait is a <strong>land where resilience thrives</strong>, and this resilience is embodied by     the enduring Sidra tree. Its roots anchor deeply in the soil, while its branches reach out, weaving together the <strong>voices of over 100 nationalities</strong>.",
+  },
+];
+
+export const nationalities = [
+  { color: "#FC9918", name: "Kuwait", percent: "32%", pathName: "kuwait" },
+  { color: "#FFC982", name: "India", percent: "21%", pathName: "india" },
+  { color: "#A92922", name: "Egypt", percent: "13%", pathName: "egypt" },
+  {
+    color: "#E15F58",
+    name: "Bangladesh",
+    percent: "6%",
+    pathName: "bangladesh",
+  },
+  {
+    color: "#D9AC86",
+    name: "Philippines",
+    percent: "5%",
+    pathName: "philippines",
+  },
+  {
+    color: "#C77024",
+    name: "Saudi Arabia",
+    percent: "3%",
+    pathName: "saudi_arabia",
+  },
+  { color: "#53A890", name: "Sri Lanka", percent: "3%", pathName: "sri_lanka" },
+  { color: "#3A7ED9", name: "Syria", percent: "3%", pathName: "syria" },
+  { color: "#84A9F0", name: "Nepal", percent: "2%", pathName: "nepal" },
+  { color: "#A9DFE6", name: "Pakistan", percent: "2%", pathName: "pakistan" },
+  { color: "#B9B9B9", name: "Others", percent: "10%", pathName: "others" },
+];
