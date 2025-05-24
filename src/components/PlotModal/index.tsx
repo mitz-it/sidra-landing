cafa193bf711b3531e529d63e207853f783dd45d
hpozza <henriquepza@gmail.com>
2025-05-24 18:37:09 -0300

    FIX/ added rem to fixed unit values

--- src/components/PlotModal/index.tsx
+++ src/components/PlotModal/index.tsx
@@ -28,9 +28,9 @@ export const PlotModal: React.FC = () => {
   );
   const [hovered, setHovered] = useState(false);
   const getModalDimensions = () => {
-    if (activeSlide === 0) return { height: "600px", width: "1000px" };
-    if (activeSlide === 1) return { height: "600px", width: "700px" };
-    if (activeSlide === 2) return { height: "500px", width: "700px" };
+    if (activeSlide === 0) return { height: "37.5rem", width: "62.5rem" };
+    if (activeSlide === 1) return { height: "37.5rem", width: "43.75rem" };
+    if (activeSlide === 2) return { height: "31.25rem", width: "43.75rem" };
     return { height: "min(54.125rem, 80vh)", width: "min(87.5rem, 90vw)" };
   };
   return (
@@ -47,7 +47,6 @@ export const PlotModal: React.FC = () => {
         }}
         onSwiper={(swiper) => setActiveSlide(swiper.activeIndex)}
         freeMode={true}
-        // onReachEnd={() => navigate('/map')}
         mousewheel
       >
         <SwiperSlide
@@ -56,16 +55,16 @@ export const PlotModal: React.FC = () => {
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "space-evenly",
-            gap: "20px",
+            gap: "1.25rem",
           }}
         >
           <SmallSpecialModalTitle>
             A place is more than just a pin on a map.
           </SmallSpecialModalTitle>
-            <SmallSpecialModalText>
-              It is a living canopy of experiences—a connection of people,
-              cultures, and shared memories.
-            </SmallSpecialModalText>
+          <SmallSpecialModalText>
+            It is a living canopy of experiences—a connection of people,
+            cultures, and shared memories.
+          </SmallSpecialModalText>
             <PinIcon width={100} height={100} />
         </SwiperSlide>
 
@@ -74,7 +73,7 @@ export const PlotModal: React.FC = () => {
             display: "flex",
             flexDirection: "column",
             justifyContent: "space-around",
-            gap: "20px",
+            gap: "1.25rem",
           }}
         >
           <SmallSpecialModalText>
@@ -82,16 +81,15 @@ export const PlotModal: React.FC = () => {
             experiences. They carry themark of the <strong>people</strong> we
             meet, the <strong>food</strong> we taste, and the{" "}
             <strong>memories</strong> we cherish.
-            </SmallSpecialModalText>
-            <SmallSpecialModalTitle>
-              Think of a happy memory.
-            </SmallSpecialModalTitle>
-            <SmallSpecialModalText>
-              What places come to your mind? Who are the people sharing that
-              moment with you? Which words would you use to describe this
-              feeling? And in which language would you say them?
-            </SmallSpecialModalText>
-          
+          </SmallSpecialModalText>
+          <SmallSpecialModalTitle>
+            Think of a happy memory.
+          </SmallSpecialModalTitle>
+          <SmallSpecialModalText>
+            What places come to your mind? Who are the people sharing that
+            moment with you? Which words would you use to describe this
+            feeling? And in which language would you say them?
+          </SmallSpecialModalText>
         </SwiperSlide>
 
         <SwiperSlide
@@ -99,7 +97,7 @@ export const PlotModal: React.FC = () => {
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
-            gap: "20px",
+            gap: "1.25rem",
           }}
         >
           <SmallSpecialModalTitle>
@@ -195,8 +193,8 @@ export const PlotModal: React.FC = () => {
             <strong>limited rights:</strong> no permanent residency,
             restricted access to healthcare, and citizenship that remains
             unattainable without a{" "}
-              <strong>direct Kuwaiti paternal bloodline</strong>—even for those
-              born in Kuwait, who may not know any other home.
+            <strong>direct Kuwaiti paternal bloodline</strong>—even for those
+            born in Kuwait, who may not know any other home.
           </SmallSpecialModalText>
         </SwiperSlide>
 
@@ -205,18 +203,18 @@ export const PlotModal: React.FC = () => {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          gap: "20px",
+          gap: "1.25rem",
         }}>
-            <ModalText
-              dangerouslySetInnerHTML={{ __html: plot[2].description }}
-              highlightColor="#FC9918"
-              style={{ fontSize: 24 }}
-            />
-            <Plot
-              imagePath={`/src/assets/plots/bornRoot/root.webp`}
-              width="1000px"
-              height="500px"
-            />
+          <ModalText
+            dangerouslySetInnerHTML={{ __html: plot[2].description }}
+            highlightColor="#FC9918"
+            style={{ fontSize: "1.5rem" }}
+          />
+          <Plot
+            imagePath={`/src/assets/plots/bornRoot/root.webp`}
+            width="62.5rem"
+            height="31.25rem"
+          />
         </SwiperSlide>
 
         <SwiperSlide>
@@ -224,12 +222,12 @@ export const PlotModal: React.FC = () => {
             <ModalText
               dangerouslySetInnerHTML={{ __html: plot[3].description }}
               highlightColor="#FC9918"
-              style={{ fontSize: 24 }}
+              style={{ fontSize: "1.5rem" }}
             />
             <Plot
               imagePath={`/src/assets/plots/residentRoot/root.webp`}
-              width="1200px"
-              height="500px"
+              width="75rem"
+              height="31.25rem"
             />
           </ModalContent>
         </SwiperSlide>
@@ -239,14 +237,14 @@ export const PlotModal: React.FC = () => {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          gap: "20px",
+          gap: "1.25rem",
         }}>
-            <ModalText
-              dangerouslySetInnerHTML={{ __html: plot[4].description }}
-              highlightColor="#cab8a0"
-              style={{ margin: "0 200px" }}
-            />
-            <Plot imagePath={`/src/assets/plots/positivityTree/tree.webp`} />
+          <ModalText
+            dangerouslySetInnerHTML={{ __html: plot[4].description }}
+            highlightColor="#cab8a0"
+            style={{ margin: "0 12.5rem" }}
+          />
+          <Plot imagePath={`/src/assets/plots/positivityTree/tree.webp`} />
         </SwiperSlide>
       </Swiper>
       <SkipButton onClick={() => navigate("/map")}>
--- src/components/PlotModal/styles.ts
+++ src/components/PlotModal/styles.ts
@@ -4,11 +4,11 @@ export const Modal = styled.div<{
   dimensions?: { height: string; width: string };
 }>`
   font-family: "Overused Grotesk", sans-serif;
-  width: ${(props) => props.dimensions?.width ?? "min(87.5rem, 90vw)"};
-  height: ${(props) => props.dimensions?.height ?? "min(54.125rem, 80vh)"};
+  width: ${(props) => props.dimensions?.width ?? "90vw"};
+  height: ${(props) => props.dimensions?.height ?? '80vh'};
   padding: 2.5rem;
-  border-radius: 20px;
-  border: 0.5px solid #00171f;
+  border-radius: 1.25rem;
+  border: 0.03125rem solid #00171f;
   background: #fff8f1;
   box-shadow: 0px 6px 0px 0px #00171f;
   position: relative;
@@ -20,20 +20,20 @@ export const Modal = styled.div<{
 
 export const ModalText = styled.p<{ highlightColor?: string }>`
   color: #00171f;
-  font-size: 32px;
+  font-size: 2rem;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
   text-align: center;
-  margin: 0 20px;
+  margin: 0 1.25rem;
 
   ${({ highlightColor }) =>
     highlightColor &&
     `
     mark {
       background: ${highlightColor};
-      padding: 0 6px;
-      border-radius: 10px;
+      padding: 0 0.375rem;
+      border-radius: 0.625rem;
       font-weight: 700;
     }
   `}
@@ -44,19 +44,19 @@ export const ModalContent = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 50px;
+  gap: 3.125rem;
 `;
 
 export const SmallSpecialModalTitle = styled.div`
   text-align: center;
-  font-size: 44px;
+  font-size: 2.75rem;
   font-family: "Zarathustra", sans-serif;
   font-weight: 400;
   line-height: normal;
 `;
 
 export const SmallSpecialModalText = styled.div`
-  font-size: 28px;
+  font-size: 1.75rem;
   font-family: "Overused Grotesk", sans-serif;
   font-weight: 400;
   line-height: normal;
@@ -68,8 +68,8 @@ export const Plot = styled.div<{
   width?: string;
   height?: string;
 }>`
-  width: ${(props) => props.width ?? "400px"};
-  height: ${(props) => props.height ?? "400px"};
+  width: ${(props) => props.width ?? "25rem"};
+  height: ${(props) => props.height ?? "25rem"};
   background-image: url(${(props) => props.imagePath});
   background-size: contain;
   background-position: center;
@@ -80,13 +80,13 @@ export const Plot = styled.div<{
 
 export const PlotLegendContainer = styled.div`
   position: relative;
-  top: 50px;
-  left: -200px;
+  top: 3.125rem;
+  left: -12.5rem;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  gap: 5px;
+  gap: 0.3125rem;
 `;
 
 export const PlotLegendItem = styled.div`
@@ -95,31 +95,31 @@ export const PlotLegendItem = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  gap: 5px;
+  gap: 0.3125rem;
   cursor: pointer;
 `;
 
 export const SkipButton = styled.div`
   color: #00171f;
   text-align: center;
-  font-size: 24px;
+  font-size: 1.5rem;
   font-style: normal;
   font-weight: 450;
   line-height: normal;
   text-decoration-line: underline;
   text-decoration-style: solid;
   position: absolute;
-  left: 20px;
-  bottom: 20px;
+  left: 1.25rem;
+  bottom: 1.25rem;
   cursor: pointer;
 `;
 
 export const InfoIcon = styled.div`
-  width: 30px;
-  height: 30px;
+  width: 1.875rem;
+  height: 1.875rem;
   border-radius: 50%;
-  margin-right: 10px;
-  border: 0.5px solid #00171f;
+  margin-right: 0.625rem;
+  border: 0.03125rem solid #00171f;
   background: #fff8f1;
   box-shadow: 0px 2px 0px 0px #00171f;
   display: flex;
@@ -127,7 +127,7 @@ export const InfoIcon = styled.div`
   justify-content: center;
   cursor: pointer;
   font-family: "Zarathustra", sans-serif;
-  font-size: 16px;
+  font-size: 1rem;
   font-weight: 700;
   color: #00171f;
   transition: transform 0.2s ease-in-out;
@@ -135,20 +135,20 @@ export const InfoIcon = styled.div`
 
 export const InfoTooltip = styled.div`
   position: absolute;
-  top: 40px; /* adjust as needed */
-  right: 0px;
+  top: 2.5rem;
+  right: 0;
   z-index: 10;
   background: #fff8f1;
   color: #00171f;
-  border: 0.5px solid #00171f;
-  border-radius: 16px;
+  border: 0.03125rem solid #00171f;
+  border-radius: 1rem;
   box-shadow: 0px 6px 0px 0px #00171f;
-  padding: 14px 10px;
-  font-size: 18px;
+  padding: 0.875rem 0.625rem;
+  font-size: 1.125rem;
   font-family: "Overused Grotesk", sans-serif;
-  max-width: 350px;
-  min-width: 250px;
+  max-width: 21.875rem;
+  min-width: 15.625rem;
   line-height: 1.4;
   text-align: left;
-  pointer-events: none; /* so it doesn't block hover */
+  pointer-events: none;
 `;
